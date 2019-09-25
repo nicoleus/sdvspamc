d80b9c3f9ef55762b6da2a06acbe6a6388ad0a06
Nicoleus1 <55134353+nicoleus@users.noreply.github.com>
2019-09-25 21:08:54 +0700

    spammer.py

---
+++ spammer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# - Spammer v2
+# | Description: spams a phone number by sending it a lot of sms by using Grab API
+# | Author: Nicoleus F Sitorus
+# | Date: 25/09/2019
+# | Disclaimer: Editing author will not make you the real coder
+
+import spammer_class
+spammer = spammer_class.Spammer()
+spammer.author = "Nicoleus F Sitorus"
+spammer.main()
